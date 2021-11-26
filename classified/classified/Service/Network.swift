8d7ae779a84f795c3d8400ff7229003abf7256b8
nikhil <nikhil.nangia@nykaa.com>
2021-11-27 00:18:24 +0530

    Added Product Detail Page With Complete implementation 1. Added a support to initialise the PDP with view model object 2. Added Safe Area support to fill super view method

--- classified/classified/Service/Network.swift
+++ classified/classified/Service/Network.swift
@@ -7,7 +7,3 @@
 
 import Foundation
 import Alamofire
-//
-//Added Product Detail Page With Complete implementation
-//1. Added a support to initialise the PDP with view model object
-//2. Added Safe Area support to fill super view method
