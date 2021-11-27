bc776effde14afb9cf7647eac3d5e2471149d9cd
nikhil <nikhil.nangia@nykaa.com>
2021-11-27 19:16:21 +0530

    Added a dependency manager based cache management

--- classified/classified/Service/ImageCache/ImageCache.swift
+++ classified/classified/Service/ImageCache/ImageCache.swift
@@ -9,34 +9,28 @@ import UIKit.UIImage
 //import Combine
 
 // Declares in-memory image cache
-public protocol ImageCacheType: class {
+public protocol ImageCacheType: AnyObject {
     // Returns the image associated with a given url
-    func image(for url: URL) -> UIImage?
+    func image(for url: URL) -> Data?
     // Inserts the image of the specified url in the cache
-    func insertImage(_ image: UIImage?, for url: URL)
+    func insertImage(_ data: Data?, for url: URL)
     // Removes the image of the specified url in the cache
     func removeImage(for url: URL)
     // Removes all images from the cache
     func removeAllImages()
     // Accesses the value associated with the given key for reading and writing
-    subscript(_ url: URL) -> UIImage? { get set }
+    subscript(_ url: URL) -> Data? { get set }
 }
 
 public final class ImageCache: ImageCacheType {
 
     // 1st level cache, that contains encoded images
-    private lazy var imageCache: NSCache<AnyObject, AnyObject> = {
-        let cache = NSCache<AnyObject, AnyObject>()
+    private lazy var imageCache: NSCache<NSString, NSData> = {
+        let cache = NSCache<NSString, NSData>()
         cache.countLimit = config.countLimit
         return cache
     }()
-    // 2nd level cache, that contains decoded images
-    private lazy var decodedImageCache: NSCache<AnyObject, AnyObject> = {
-        let cache = NSCache<AnyObject, AnyObject>()
-        cache.totalCostLimit = config.memoryLimit
-        return cache
-    }()
-    private let lock = NSLock()
+   
     private let config: Config
 
     public struct Config {
@@ -50,67 +44,34 @@ public final class ImageCache: ImageCacheType {
         self.config = config
     }
 
-    public func image(for url: URL) -> UIImage? {
-        lock.lock(); defer { lock.unlock() }
-        // the best case scenario -> there is a decoded image in memory
-        if let decodedImage = decodedImageCache.object(forKey: url as AnyObject) as? UIImage {
-            return decodedImage
-        }
-        // search for image data
-        if let image = imageCache.object(forKey: url as AnyObject) as? UIImage {
-            let decodedImage = image.decodedImage()
-            decodedImageCache.setObject(image as AnyObject, forKey: url as AnyObject, cost: decodedImage.diskSize)
-            return decodedImage
+    public func image(for url: URL) -> Data? {
+        if let imageData = imageCache.object(forKey: url.absoluteString as NSString) {
+          print("using cached images")
+          return imageData as Data
         }
         return nil
     }
 
-    public func insertImage(_ image: UIImage?, for url: URL) {
-        guard let image = image else { return removeImage(for: url) }
-        let decompressedImage = image.decodedImage()
-
-        lock.lock(); defer { lock.unlock() }
-        imageCache.setObject(decompressedImage, forKey: url as AnyObject, cost: 1)
-        decodedImageCache.setObject(image as AnyObject, forKey: url as AnyObject, cost: decompressedImage.diskSize)
+    public func insertImage(_ data: Data?, for url: URL) {
+        if let result = data as NSData? {
+            self.imageCache.setObject(result, forKey: url.absoluteString as NSString)
+        }
     }
 
     public func removeImage(for url: URL) {
-        lock.lock(); defer { lock.unlock() }
-        imageCache.removeObject(forKey: url as AnyObject)
-        decodedImageCache.removeObject(forKey: url as AnyObject)
+        imageCache.removeObject(forKey: url.absoluteString as NSString)
     }
 
     public func removeAllImages() {
-        lock.lock(); defer { lock.unlock() }
         imageCache.removeAllObjects()
-        decodedImageCache.removeAllObjects()
     }
 
-    public subscript(_ key: URL) -> UIImage? {
+    public subscript(_ key: URL) -> Data? {
         get {
             return image(for: key)
         }
         set {
             return insertImage(newValue, for: key)
         }
-    }
-}
-
-fileprivate extension UIImage {
-
-    func decodedImage() -> UIImage {
-        guard let cgImage = cgImage else { return self }
-        let size = CGSize(width: cgImage.width, height: cgImage.height)
-        let colorSpace = CGColorSpaceCreateDeviceRGB()
-        let context = CGContext(data: nil, width: Int(size.width), height: Int(size.height), bitsPerComponent: 8, bytesPerRow: cgImage.bytesPerRow, space: colorSpace, bitmapInfo: CGImageAlphaInfo.noneSkipFirst.rawValue)
-        context?.draw(cgImage, in: CGRect(origin: .zero, size: size))
-        guard let decodedImage = context?.makeImage() else { return self }
-        return UIImage(cgImage: decodedImage)
-    }
-
-    // Rough estimation of how much memory image uses in bytes
-    var diskSize: Int {
-        guard let cgImage = cgImage else { return 0 }
-        return cgImage.bytesPerRow * cgImage.height
     }
 }
--- classified/classified/Service/ImageCache/ImageLoader.swift
+++ classified/classified/Service/ImageCache/ImageLoader.swift
@@ -8,56 +8,49 @@ import Foundation
 import UIKit
 
 enum NetworkManagerError: Error {
-  case badResponse(URLResponse?)
-  case badData
-  case badLocalUrl
+    case badResponse(URLResponse?)
+    case badData
+    case badLocalUrl
 }
 
 public final class ImageLoader {
-
+    private let cache: ImageCacheType
     static var shared = ImageLoader()
-    
-    private var images = NSCache<NSString, NSData>()
-    
     let session: URLSession
-    init() {
+    
+    init(cache: ImageCacheType = ImageCache()) {
+        self.cache = cache
         let config = URLSessionConfiguration.default
         session = URLSession(configuration: config)
-      }
-      
+    }
     private func download(imageURL: URL, completion: @escaping (Data?, Error?) -> (Void)) {
-      if let imageData = images.object(forKey: imageURL.absoluteString as NSString) {
-        print("using cached images")
-        completion(imageData as Data, nil)
-        return
-      }
-      
-      let task = session.downloadTask(with: imageURL) { localUrl, response, error in
-        if let error = error {
-          completion(nil, error)
-          return
+        if let imageData = cache.image(for: imageURL) {
+            print("Image Data from cache manger \(imageData)")
+            completion(imageData as Data, nil)
+            return
         }
-        
-        guard let httpResponse = response as? HTTPURLResponse, (200...299).contains(httpResponse.statusCode) else {
-          completion(nil, NetworkManagerError.badResponse(response))
-          return
+        let task = session.downloadTask(with: imageURL) { localUrl, response, error in
+            if let error = error {
+                completion(nil, error)
+                return
+            }
+            guard let httpResponse = response as? HTTPURLResponse, (200...299).contains(httpResponse.statusCode) else {
+                completion(nil, NetworkManagerError.badResponse(response))
+                return
+            }
+            guard let localUrl = localUrl else {
+                completion(nil, NetworkManagerError.badLocalUrl)
+                return
+            }
+            do {
+                let data = try Data(contentsOf: localUrl)
+                self.cache.insertImage(data, for: imageURL)
+                completion(data, nil)
+            } catch let error {
+                completion(nil, error)
+            }
         }
-        
-        guard let localUrl = localUrl else {
-          completion(nil, NetworkManagerError.badLocalUrl)
-          return
-        }
-        
-        do {
-          let data = try Data(contentsOf: localUrl)
-          self.images.setObject(data as NSData, forKey: imageURL.absoluteString as NSString)
-          completion(data, nil)
-        } catch let error {
-          completion(nil, error)
-        }
-      }
-      
-      task.resume()
+        task.resume()
     }
     
     func image(product: Product, completion: @escaping (Data?, Error?) -> (Void)) {
